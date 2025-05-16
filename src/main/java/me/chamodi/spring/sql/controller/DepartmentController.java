981310b9febc6475faace443adeacb63bbba9935
Chamodi Indrejith <chamodiindrejith14@gmail.com>
2025-05-16 09:58:23 +0530

    feat: update and delete department

--- src/main/java/me/chamodi/spring/sql/controller/DepartmentController.java
+++ src/main/java/me/chamodi/spring/sql/controller/DepartmentController.java
@@ -8,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import me.chamodi.spring.sql.models.Department;
 import me.chamodi.spring.sql.repo.DepartmentRepo;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 
@@ -29,4 +36,28 @@ public class DepartmentController {
         return repo.findById(id).get();
     }
     
+    @PostMapping
+    public String addDept(@RequestBody Department department) {
+        repo.save(department);
+        return "New Department Created!";
+    }
+    
+    @PutMapping("/{id}")
+    public String updateDept(@PathVariable String id, @RequestBody Department department) {
+        if(repo.findById(id).isEmpty()){
+            return "Couldn't find the department!";
+        }
+        repo.save(department);
+        return "Updated Successfully!";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteDept(@PathVariable("id") String id) {
+        if(repo.findById(id).isEmpty()){
+            return "Couldn't find the department!";
+        }
+        repo.deleteById(id);;
+        return "Department Deleted Successfully!";
+    }
+
 }
